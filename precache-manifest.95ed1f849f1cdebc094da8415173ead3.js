0c97facfa666a9414b86c8a35f03bd26cd548332
SaravanakumarVijay <saravanakumar.tsm@gmail.com>
2021-08-29 15:32:41 +0000

    Deploying to gh-pages from  @ 55a76d61ceb2552e5312dc3f6b3a7fe3dc39bdcb 🚀

---
+++ precache-manifest.95ed1f849f1cdebc094da8415173ead3.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3fe2cbb07588dbd13be5e2b18fca6e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "55432bcbf61b3de49668",
+    "url": "/Portfolio/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "c6893fa53c6071bb0beb",
+    "url": "/Portfolio/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "7535af470a8eeeae956b",
+    "url": "/Portfolio/static/css/main.c037c4c9.chunk.css"
+  },
+  {
+    "revision": "aab05ad817284e669e1e",
+    "url": "/Portfolio/static/js/2.e68a0a4c.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/Portfolio/static/js/2.e68a0a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55432bcbf61b3de49668",
+    "url": "/Portfolio/static/js/3.32901358.chunk.js"
+  },
+  {
+    "revision": "c6893fa53c6071bb0beb",
+    "url": "/Portfolio/static/js/4.772b6f32.chunk.js"
+  },
+  {
+    "revision": "7535af470a8eeeae956b",
+    "url": "/Portfolio/static/js/main.cf935902.chunk.js"
+  },
+  {
+    "revision": "d47dc3faa96cf95bef35",
+    "url": "/Portfolio/static/js/runtime-main.2805eba7.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/Portfolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/Portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "609378665c4fc994d7c17cc187edb496",
+    "url": "/Portfolio/static/media/debris.60937866.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/Portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "925057e556cc81bf5043c98956debaf6",
+    "url": "/Portfolio/static/media/ii.925057e5.png"
+  },
+  {
+    "revision": "2a05949f8359cdaa17b0186bacf12ced",
+    "url": "/Portfolio/static/media/majestic.2a05949f.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "81f82a20367c5e1b688d36ff4b90f8ca",
+    "url": "/Portfolio/static/media/mc.81f82a20.png"
+  },
+  {
+    "revision": "1bc30dd741889496e59308aeb3744519",
+    "url": "/Portfolio/static/media/mku.1bc30dd7.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/Portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "be4008548c6a8f32fb166f1169882227",
+    "url": "/Portfolio/static/media/tsm.be400854.jpg"
+  }
+]);
